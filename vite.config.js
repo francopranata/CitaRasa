cc824a6e3fba2f1924f21588050caf3ad58acce8
Toniii67 <francopranara@gmail.com>
2025-06-23 14:44:01 +0700

    update images

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/CitaRasa/",
   plugins: [
     react(),
     tailwindcss()
   ],
-  base: "/CitaRasa",
 })
